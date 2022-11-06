08c4c28b7c75d314b0045a0ab7d9f62b52e9b0a8
LimjooHyun <99496403+uhzill@users.noreply.github.com>
2022-11-07 06:28:54 +0900

    40

--- js/script.js
+++ js/script.js
@@ -1,182 +1,185 @@
 
 $(document).ready(function(){
+
+
     $(function(){
 
         $("html").mousewheel(function(event, d){
 
             if(d > 0){//스크롤 올릴 때 
                 var scroll = $(window).scrollLeft();
-                scroll -= 500; 
+                scroll -= 350; 
             } else if(d < 0) { //스크롤 내릴 때
                 var scroll = $(window).scrollLeft();
-                scroll += 500;
+                scroll += 350;
             }
             $("html, body").stop().animate({
                     "scrollLeft":scroll
             },1);
+
+            
         });
+        
+
     });
 
-//현재 스크롤 위치
-// window.addEventListener('scroll', () => { 
-// console.log(window.scrollX+","+ window.scrollY);
-// });
 
 
-// 물결
-$(".gradientBg").ripples({
-    resolution: 250, // 값이 클수록 잔물결 느리게 전파
-    perturbance: .001, // 잔물경 굴절 강도. (0은 굴절 없음)
-});
+    // 물결
+    $(".gradientBg").ripples({
+        resolution: 250, // 값이 클수록 잔물결 느리게 전파
+        perturbance: .001, // 잔물경 굴절 강도. (0은 굴절 없음)
+    });
 
 
-// 별
-var c = document.getElementById("canv");
-var d = c.getContext("2d");
-var w = c.width = window.innerWidth;
-var h = c.height = window.innerHeight;
 
-var arr = [];
-var u = 0;
-var dep = w;
-var dp = 0.70;
-var ms = { x: 0, y: 0 };
-var msd = { x: 0, y: 0 };
+    // 별
+    var c = document.getElementById("canv");
+    var d = c.getContext("2d");
+    var w = c.width = window.innerWidth;
+    var h = c.height = window.innerHeight;
 
-function Obj(x, y, z) {this.set(x, y, z);}
+    var arr = [];
+    var u = 0;
+    var dep = w;
+    var dp = 0.70;
+    var ms = { x: 0, y: 0 };
+    var msd = { x: 0, y: 0 };
 
-Obj.prototype = {
-set: function(x, y, z) {
-    this.x = x || 0;
-    this.y = y || 0;
-    this.z = z || 0;
-},
-rotX: function(ang) {
-    var y = this.y;
-    var z = this.z;
-    this.y = y * Math.cos(ang) - z * Math.sin(ang);
-    this.z = z * Math.cos(ang) + y * Math.sin(ang);
-},
-rotY: function(ang) {
-    var x = this.x;
-    var z = this.z;
-    this.x = x * Math.cos(ang) - z * Math.sin(ang);
-    this.z = z * Math.cos(ang) + x * Math.sin(ang);
-},
-rotZ: function(ang) {
-    var x = this.x;
-    var y = this.y;
-    this.x = x * Math.cos(ang) - y * Math.sin(ang);
-    this.y = y * Math.cos(ang) + x * Math.sin(ang);
-}
-};
+    function Obj(x, y, z) {this.set(x, y, z);}
 
-function Part(x, y, z) {
-this.op = new Obj(x, y, z);
-this.rp = new Obj(x, y, z);
-this.rot = new Obj();
-this.vel = new Obj();
-this.col = 'hsla(216,95%,85%,'+rnd(0.5, 1)+')';
-}
+    Obj.prototype = {
+    set: function(x, y, z) {
+        this.x = x || 0;
+        this.y = y || 0;
+        this.z = z || 0;
+    },
+    rotX: function(ang) {
+        var y = this.y;
+        var z = this.z;
+        this.y = y * Math.cos(ang) - z * Math.sin(ang);
+        this.z = z * Math.cos(ang) + y * Math.sin(ang);
+    },
+    rotY: function(ang) {
+        var x = this.x;
+        var z = this.z;
+        this.x = x * Math.cos(ang) - z * Math.sin(ang);
+        this.z = z * Math.cos(ang) + x * Math.sin(ang);
+    },
+    rotZ: function(ang) {
+        var x = this.x;
+        var y = this.y;
+        this.x = x * Math.cos(ang) - y * Math.sin(ang);
+        this.y = y * Math.cos(ang) + x * Math.sin(ang);
+    }
+    };
 
-function upd(rot) {
-var pos;
-var rot;
-var vel;
-var op;
-var rp;
-var col;
-var size;
+    function Part(x, y, z) {
+    this.op = new Obj(x, y, z);
+    this.rp = new Obj(x, y, z);
+    this.rot = new Obj();
+    this.vel = new Obj();
+    this.col = 'hsla(216,95%,85%,'+rnd(0.5, 1)+')';
+    }
 
-for (var i in arr) {
-    op = arr[i].op;
-    rp = arr[i].rp;
-    rot = arr[i].rot;
-    vel = arr[i].vel;
-    col = arr[i].col;
-    vel.x += msd.x * 0.15;
-    vel.y += msd.y * 0.15;
-    rp.set(op.x, op.y, op.z);
+    function upd(rot) {
+    var pos;
+    var rot;
+    var vel;
+    var op;
+    var rp;
+    var col;
+    var size;
 
-    rot.x += vel.x;
-    rot.y += vel.y;
-    rot.z += vel.z;
-
-    rot.x = rot.x > Math.PI * 2 ? 0 : rot.x;
-    rot.y = rot.y > Math.PI * 2 ? 0 : rot.y;
-
-    rp.rotY(rot.y);
-    rp.rotX(rot.x);
-
-    vel.set(
-    vel.x * dp,
-    vel.y * dp,
-    0
-    );
-}
-msd.x = 0.0005;
-msd.y = 0.0005;
-}
-
-function draw() {
-    var p, dth;
     for (var i in arr) {
-        p = arr[i];
-        dth = ((p.rp.z / dep) + 1);
-        d.fillStyle = p.col;
-        d.fillRect(w + p.rp.x, h + p.rp.y, rnd(dth/0.8, dth/2),  dth/0.9);
-    }
-}
+        op = arr[i].op;
+        rp = arr[i].rp;
+        rot = arr[i].rot;
+        vel = arr[i].vel;
+        col = arr[i].col;
+        vel.x += msd.x * 0.15;
+        vel.y += msd.y * 0.15;
+        rp.set(op.x, op.y, op.z);
 
-function rnd(min, max) {
-    return Math.random() * (max - min) + min;
-}
+        rot.x += vel.x;
+        rot.y += vel.y;
+        rot.z += vel.z;
 
-function go() {
-    for (var i = 0; i < 6800; i++) {
-        var d = new Part(
-        rnd(-w, h),
-        rnd(-w, h),
-        rnd(-dep, dep)
+        rot.x = rot.x > Math.PI * 2 ? 0 : rot.x;
+        rot.y = rot.y > Math.PI * 2 ? 0 : rot.y;
+
+        rp.rotY(rot.y);
+        rp.rotX(rot.x);
+
+        vel.set(
+        vel.x * dp,
+        vel.y * dp,
+        0
         );
-        d.vel.set(0, 0, 0);
-        arr.push(d);
     }
-}
+    msd.x = 0.0005;
+    msd.y = 0.0005;
+    }
 
-window.addEventListener('mousemove', function(e) {
-    msd.x = (e.clientY - ms.y) / w;
-    msd.y = (e.clientX - ms.x) / h;
-    ms.x = e.clientX;
-    ms.y = e.clientY;
-}, false);
+    function draw() {
+        var p, dth;
+        for (var i in arr) {
+            p = arr[i];
+            dth = ((p.rp.z / dep) + 1);
+            d.fillStyle = p.col;
+            d.fillRect(w + p.rp.x, h + p.rp.y, rnd(dth/0.8, dth/2),  dth/0.9);
+        }
+    }
 
-window.addEventListener('touchmove', function(e) {
-    e.preventDefault();
-    msd.x = (e.touches[0].clientY - ms.y) / w;
-    msd.y = (e.touches[0].clientX - ms.x) / h;
-    ms.x = e.touches[0].clientX;
-    ms.y = e.touches[0].clientY;
-}, false);
+    function rnd(min, max) {
+        return Math.random() * (max - min) + min;
+    }
 
-window.addEventListener('resize', function(e) {
-    c.width = w = window.innerWidth;
-    c.height = h = window.innerHeight;
-}, false);
+    function go() {
+        for (var i = 0; i < 6800; i++) {
+            var d = new Part(
+            rnd(-w, h),
+            rnd(-w, h),
+            rnd(-dep, dep)
+            );
+            d.vel.set(0, 0, 0);
+            arr.push(d);
+        }
+    }
 
-function run() {
-    d.clearRect(0, 0, w, h);
-    var g_ = d.createLinearGradient(c.width + c.width,
-        c.height + c.height * 1.5,
-        c.width + c.width, 1);
+    window.addEventListener('mousemove', function(e) {
+        msd.x = (e.clientY - ms.y) / w;
+        msd.y = (e.clientX - ms.x) / h;
+        ms.x = e.clientX;
+        ms.y = e.clientY;
+    }, false);
 
-    upd();
-    draw();
-    window.requestAnimationFrame(run);
-}
+    window.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+        msd.x = (e.touches[0].clientY - ms.y) / w;
+        msd.y = (e.touches[0].clientX - ms.x) / h;
+        ms.x = e.touches[0].clientX;
+        ms.y = e.touches[0].clientY;
+    }, false);
 
-go();
-run();
+    window.addEventListener('resize', function(e) {
+        c.width = w = window.innerWidth;
+        c.height = h = window.innerHeight;
+    }, false);
+
+    function run() {
+        d.clearRect(0, 0, w, h);
+        var g_ = d.createLinearGradient(c.width + c.width,
+            c.height + c.height * 1.5,
+            c.width + c.width, 1);
+
+        upd();
+        draw();
+        window.requestAnimationFrame(run);
+    }
+
+    go();
+    run();
 
 });
 
